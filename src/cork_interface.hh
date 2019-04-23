e3b8d015b7696bcbec2155e8646b40ca5f884e47
ali <ali.falsafi@epfl.ch>
2019-04-23 11:21:40 +0200

    files moved to src_corkc cfolder

--- src/cork_interface.hh
+++ src/cork_interface.hh
@@ -1,4 +1,31 @@
-#include "file_formats/files.h"
+/**
+ * @file   cork_interface.hh
+ *
+ * @author Ali Falsafi <ali.falsafi@epfl.ch>
+ *
+ * @date   23 Apr 2019
+ *
+ * @brief This interface is used to interact with cork package for calculating
+ * intesecting voulme and normal vector of the corrspondent interface
+ *
+ * Copyright © 2017 Till Junge
+ *
+ * µSpectre is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * µSpectre is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 #include <iostream>
